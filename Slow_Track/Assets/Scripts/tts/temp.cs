9637f7ebe23c985b8da16310a27e3f999afd86b5
max4779 <126331668+max4779@users.noreply.github.com>
2025-12-19 19:25:11 +0900

    TTS 영어기준으로 변경
    TTS 영어기준으로 변경 및 stop버튼 누를시 출력(임시)

---
+++ Slow_Track/Assets/Scripts/tts/temp.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class temp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        WinTTS.Speak("Sunday morning rain is falling. Steal some covers.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
